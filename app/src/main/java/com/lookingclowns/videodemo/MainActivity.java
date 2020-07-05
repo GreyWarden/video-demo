5cb783a3bbd6b00ad07745f600491a11c34e1f38
Raúl Pavón <raulpavonfp@gmail.com>
2020-07-05 20:05:44 +0200

    Added mediacontroller

--- app/src/main/java/com/lookingclowns/videodemo/MainActivity.java
+++ app/src/main/java/com/lookingclowns/videodemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lookingclowns.videodemo;
 
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         VideoView video = findViewById(R.id.videoView);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(video);
+        video.setMediaController(mediaController);
         video.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
         video.start();
     }
